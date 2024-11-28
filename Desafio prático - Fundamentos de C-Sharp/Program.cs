2c37c2bf89083ff4bc6bc2a49805b7a8d078fd46
Davi da Silva dos Santos <davi.santos@scoreplan.com.br>
2024-11-28 17:05:10 -0300

    Crie um programa em que o usuário precisa digitar um nome e uma mensagem de boas vindas personalizada com o nome dele é exibida - Desáfio 1

--- Desafio prático - Fundamentos de C-Sharp/Program.cs
+++ Desafio prático - Fundamentos de C-Sharp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Diga-me o seu nome, para que possamos criar uma mensagem modificável");
+        string? name = Console.ReadLine();
+
+        while(name is not string || string.IsNullOrEmpty(name) || (!string.IsNullOrEmpty(name) && name.Trim().Length == 0))
+        {
+            Console.WriteLine("Diga-me, novamente, o seu nome, pois houve algum erro");
+            name = Console.ReadLine();
+        }
+
+        name = name.Trim();
+
+        Console.WriteLine($"Olá {name}! Seja muito bem-vindo!");
+        Console.WriteLine(string.Format("Ola {0}! Seja muito bem-vindo!", name));
+        Console.WriteLine("Ola " + name + "! Seja muito bem-vindo!");
+        StringBuilder sb = new StringBuilder("Olá ");
+        sb.Append(name);
+        sb.Append("! Seja muito bem-vindo!");
+        Console.WriteLine(sb.ToString());
+    }
+}
