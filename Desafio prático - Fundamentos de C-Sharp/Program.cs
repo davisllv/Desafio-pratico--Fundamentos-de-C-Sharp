e226aa50f987c172b2f93e0aebfad5646ff3d258
Davi da Silva dos Santos <davi.santos@scoreplan.com.br>
2024-11-28 17:13:50 -0300

    Crie um programa que concatene um nome e um sobrenome inseridos pelo usuário - Desafio 2

--- Desafio prático - Fundamentos de C-Sharp/Program.cs
+++ Desafio prático - Fundamentos de C-Sharp/Program.cs
@@ -1,19 +1,23 @@
 ﻿using System.Text;
+using System.Xml.Linq;
 
 class Program
 {
-    public static void Main()
+    public static string ValidateSchemaNameSurname(string? nameOrSurname)
     {
-        Console.WriteLine("Diga-me o seu nome, para que possamos criar uma mensagem modificável");
-        string? name = Console.ReadLine();
-
-        while(name is not string || string.IsNullOrEmpty(name) || (!string.IsNullOrEmpty(name) && name.Trim().Length == 0))
+        string? nameOrSurnameValidate = nameOrSurname;
+        while (nameOrSurnameValidate is not string || string.IsNullOrEmpty(nameOrSurnameValidate) || (!string.IsNullOrEmpty(nameOrSurnameValidate) && nameOrSurnameValidate.Trim().Length == 0))
         {
-            Console.WriteLine("Diga-me, novamente, o seu nome, pois houve algum erro");
-            name = Console.ReadLine();
+            Console.WriteLine("Diga-me, novamente pois houve algum erro");
+            nameOrSurnameValidate = Console.ReadLine();
         }
 
-        name = name.Trim();
+        return nameOrSurnameValidate.Trim();
+    }
+    public static void Desafio1()
+    {
+        Console.WriteLine("Diga-me o seu nome, para que possamos criar uma mensagem modificável");
+        string name = ValidateSchemaNameSurname(Console.ReadLine());
 
         Console.WriteLine($"Olá {name}! Seja muito bem-vindo!");
         Console.WriteLine(string.Format("Ola {0}! Seja muito bem-vindo!", name));
@@ -22,5 +26,18 @@ class Program
         sb.Append(name);
         sb.Append("! Seja muito bem-vindo!");
         Console.WriteLine(sb.ToString());
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Diga-me o seu nome, para que possamos criar uma mensagem modificável");
+        string name = ValidateSchemaNameSurname(Console.ReadLine());
+
+        Console.WriteLine("Agora, diga-me o seu sobrenome");
+        string? surname = ValidateSchemaNameSurname(Console.ReadLine());
+
+        Console.WriteLine($"Olá {name} {surname}! Seja muito bem-vindo!");
+        Console.WriteLine(string.Format("Ola {0} {1}! Seja muito bem-vindo!", name, surname));
+        Console.WriteLine("Ola " + name + " " + surname + "! Seja muito bem-vindo!");
     }
 }
